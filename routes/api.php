8c5b2a0ade5cf4ac8b2d0de001335ca1b973f516
icedhydro <140410752+icedhydro@users.noreply.github.com>
2025-02-12 15:06:45 +0100

    Fixed API route order to correctly register product search, filter, and price history

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
 Route::middleware('api')->group(function () {
-    Route::apiResource('products', ProductController::class);
     Route::get('products/{product}/price-history', [ProductController::class, 'priceHistory']);
     Route::get('products/search', [ProductController::class, 'search']);
     Route::get('products/filter', [ProductController::class, 'filter']);
+    Route::apiResource('products', ProductController::class);
 });
