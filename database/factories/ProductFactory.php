ccf9591531ea238318794f60e96bad29cbbb90de
icedhydro <140410752+icedhydro@users.noreply.github.com>
2025-02-12 15:31:35 +0100

    Updated ProductFactory to generate realistic products

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -11,10 +11,16 @@ class ProductFactory extends Factory
 
     public function definition()
     {
+        $fruits = ['Jablko', 'Hruška', 'Banán', 'Pomeranč', 'Jahoda', 'Meloun', 'Švestka', 'Meruňka', 'Višeň', 'Hrozny'];
+        $nuts = ['Vlašský ořech', 'Lískový ořech', 'Mandle', 'Kešu', 'Pekanový ořech', 'Para ořech'];
+        $vegetables = ['Mrkev', 'Okurka', 'Rajče', 'Paprika', 'Brambory', 'Cibule', 'Česnek', 'Dýně'];
+
+        $products = array_merge($fruits, $nuts, $vegetables);
+
         return [
-            'name' => $this->faker->word,
-            'price' => $this->faker->randomFloat(2, 1, 100),
-            'stock' => $this->faker->numberBetween(1, 500),
+            'name' => $this->faker->randomElement($products),
+            'price' => $this->faker->randomFloat(2, 5, 100),
+            'stock' => $this->faker->numberBetween(10, 200),
         ];
     }
 }
