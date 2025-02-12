7fa4157e1af2046d34cd5665fdaea67cf8524c31
icedhydro <140410752+icedhydro@users.noreply.github.com>
2025-02-12 13:17:40 +0100

    Added relationship between Product and PriceHistory

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,4 +10,9 @@ class Product extends Model
     use HasFactory;
 
     protected $fillable = ['name', 'price', 'stock'];
+
+    public function priceHistory()
+    {
+        return $this->hasMany(PriceHistory::class);
+    }
 }
